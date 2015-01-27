af4d2dbe1aecd3ca67604a115cdb6e9f84a49637
vilyever <250450035@163.com>
2015-01-27 17:53:20 +0800

    Initial Commit

---
+++ VDKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VDKit
+//
+//  Created by FTET on 15/1/27.
+//  Copyright (c) 2015年 Vilyever. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
